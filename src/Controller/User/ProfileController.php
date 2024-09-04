998389dee72e8f689295de8166149942bd4be4a6
VENDEVILLE GUILLAUME <gvendeville@creacard.net>
2024-09-04 15:42:19 +0200

    reset du password d'un utilisateur et oublie de password

---
+++ src/Controller/User/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Form\ChangePasswordFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
+
+#[Route('/profil', name: 'app_profile_')]
+class ProfileController extends AbstractController
+{
+    
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    
+}
